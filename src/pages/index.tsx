de3156655466904f6cd74dea1bb155a5e0f60f60
Pawel Lisewski <pawel.lisewski@gmail.com>
2020-05-28 17:53:11 +0800

    copy data directory

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Link } from "gatsby";
 import { Button, Box, Text, Heading } from "@chakra-ui/core";
 
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import SEO from "../components/seo";
 
 const IndexPage: React.FC<{}> = () => (
